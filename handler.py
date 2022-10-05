52862d0b8f8d409165b34a0ba077233c1e2f1422
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2022-10-05 14:18:39 +0200

    remove printing the response

--- handler.py
+++ handler.py
@@ -7,7 +7,8 @@ from io import BytesIO
 from base64 import b64decode
 from botocore.exceptions import ClientError
 
-s3_client = boto3.client('s3')
+# Boto3 makes it easy to integrate your Python application, library, or script with AWS services including Amazon S3, Amazon EC2, Amazon DynamoDB, and more.
+s3_client = boto3.client('s3')  
 bucket = os.environ['BUCKET_NAME']
 
 def image_upload(event, context):
@@ -20,8 +21,7 @@ def image_upload(event, context):
         file_content.save(image, format=file_content.format)
         image.seek(0)
         response = s3_client.upload_fileobj(image, bucket, file_name)
-        print('response:::', response)
-
+        
         url = '{}/{}/{}'.format(s3_client.meta.endpoint_url, bucket, file_name)
         return url
 
